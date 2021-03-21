d5964fc09d80bb0d0b6ee5463ebf0211cc2ae0b5
Bekzat <Bekzatashken@gmail.com>
2021-03-21 14:01:07 +0600

    mainbox

--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ import { Clock } from './components/Clock/Clock'
 import { Click } from './components/Click/Click'
 import { Top } from './components/Top/Top'
 import { CheckLogin } from './components/CheckLogin/CheckLogin'
+import { Mainbox } from './components/Mainbox/Mainbox'
+
 const root = document.getElementById('root')
 
 ReactDOM.render(
   <div>
     <Clock />
+    <br/>
     <Click />
+    <br/>
     <Top />
+    <br/>
     <CheckLogin />
+    <br/>
+    <Mainbox unreadMessages={[`Liza`, `Gerbi4`]} />
   </div>
 ,
   root
